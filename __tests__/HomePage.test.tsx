858b21879d6c774ba40ece7c5b1f74523678af5b
Akshat Mittal <akshatmittal2506@gmail.com>
2023-11-20 11:27:51 +0530

    test: configured router mock for unit tests

--- __tests__/HomePage.test.tsx
+++ __tests__/HomePage.test.tsx
@@ -1,6 +1,23 @@
 /* eslint-disable no-undef */
 import HomePage from "@/pages";
 import { render } from "@testing-library/react";
+import { useRouter } from "next/router";
+
+// mock useRouter
+jest.mock("next/router", () => ({
+	useRouter: jest.fn(),
+}));
+
+// setup a new mocking function for push method
+const pushMock = jest.fn();
+
+// mock a return value on useRouter
+(useRouter as any).mockReturnValue({
+	query: {},
+	// return mock for push method
+	push: pushMock,
+	// ... add the props or methods you need
+});
 
 // To check if Home Page has text length greater than 0
 test("Home Page has text content", () => {
