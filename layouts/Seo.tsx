d03e3f4104fe85d834e7a0fa446c092cf0252111
Akshat Mittal <akshatmittal2506@gmail.com>
2024-08-28 23:59:18 +0530

    fix: base seo component

--- layouts/Seo.tsx
+++ layouts/Seo.tsx
@@ -43,7 +43,7 @@ interface ISeoProps {
 	};
 }
 
-const Seo: React.FC<ISeoProps> = ({
+export const Seo: React.FC<ISeoProps> = ({
 	title,
 	description,
 	author,
@@ -57,18 +57,19 @@ const Seo: React.FC<ISeoProps> = ({
 }) => {
 	return (
 		<Head>
-			<title>{title}</title>
-			<meta name="description" content={description} />
-			<meta name="image" content={image} />
-			<meta name="author" content={author} />
-			<meta name="site_name" content={siteName} />
-			<link rel="canonical" href={canonical} />
-			<meta name="theme-color" content={themeColor} />
+			<title key="title">{title}</title>
+			<meta key="description" name="description" content={description} />
+			<meta key="image" name="image" content={image} />
+			<meta key="author" name="author" content={author} />
+			<meta key="siteName" name="site_name" content={siteName} />
+			<link key="canonical" rel="canonical" href={canonical} />
+			<meta key="themeColor" name="theme-color" content={themeColor} />
 
 			{icons?.map((item, index) => {
 				return (
-					<React.Fragment key={index}>
+					<React.Fragment key={`head-icon-${index}`}>
 						<link
+							key={`head-icon-${index}`}
 							rel={item.rel}
 							href={item.href}
 							// sizes={item.sizes}
@@ -78,42 +79,87 @@ const Seo: React.FC<ISeoProps> = ({
 				);
 			})}
 
-			<meta property="og:title" content={og?.title} />
-			<meta property="og:description" content={og?.description} />
+			<meta key="ogTitle" property="og:title" content={og?.title} />
+			<meta
+				key="ogDescription"
+				property="og:description"
+				content={og?.description}
+			/>
 			{og?.images?.map((item, index) => {
 				return (
-					<React.Fragment key={index}>
-						<meta property="og:image" content={item?.url} />
+					<React.Fragment key={`head-og-image-${index}`}>
 						<meta
+							key="ogImage"
+							property="og:image"
+							content={item?.url}
+						/>
+						<meta
+							key="ogImageSecureUrl"
 							property="og:image:secure_url"
 							content={item?.secureUrl}
 						/>
-						<meta property="og:image:type" content={item?.type} />
 						<meta
+							key="ogImageType"
+							property="og:image:type"
+							content={item?.type}
+						/>
+						<meta
+							key="ogImageWidth"
 							property="og:image:width"
 							content={item?.width + ""}
 						/>
 						<meta
+							key="ogImageHeight"
 							property="og:image:height"
 							content={item?.height + ""}
 						/>
-						<meta property="og:image:alt" content={item?.alt} />
+						<meta
+							key="ogImageAlt"
+							property="og:image:alt"
+							content={item?.alt}
+						/>
 					</React.Fragment>
 				);
 			})}
-			<meta property="og:url" content={og?.url} />
-			<meta property="og:type" content={og?.type} />
-			<meta property="og:site_name" content={og?.siteName} />
+			<meta key="ogUrl" property="og:url" content={og?.url} />
+			<meta key="ogType" property="og:type" content={og?.type} />
+			<meta
+				key="ogSiteName"
+				property="og:site_name"
+				content={og?.siteName}
+			/>
 
-			<meta name="twitter:card" content={twitter?.card} />
-			<meta name="twitter:site" content={twitter?.site} />
-			<meta name="twitter:creator" content={twitter?.author} />
-			<meta name="twitter:title" content={twitter?.title} />
-			<meta name="twitter:description" content={twitter?.description} />
-			<meta name="twitter:image" content={twitter?.image} />
-			<meta name="twitter:url" content={twitter?.url} />
+			<meta
+				key="twitterCard"
+				name="twitter:card"
+				content={twitter?.card}
+			/>
+			<meta
+				key="twitterSite"
+				name="twitter:site"
+				content={twitter?.site}
+			/>
+			<meta
+				key="twitterCreator"
+				name="twitter:creator"
+				content={twitter?.author}
+			/>
+			<meta
+				key="twitterTitle"
+				name="twitter:title"
+				content={twitter?.title}
+			/>
+			<meta
+				key="twitterDescription"
+				name="twitter:description"
+				content={twitter?.description}
+			/>
+			<meta
+				key="twitterImage"
+				name="twitter:image"
+				content={twitter?.image}
+			/>
+			<meta key="twitterUrl" name="twitter:url" content={twitter?.url} />
 		</Head>
 	);
 };
-
-export default Seo;
--- layouts/index.ts
+++ layouts/index.ts
@@ -1,6 +1,6 @@
 import Masonry from "./Masonry";
 import Responsive from "./Responsive";
-import Seo from "./Seo";
+import { Seo } from "./Seo";
 import Wrapper from "./Wrapper";
 
 export { Masonry, Responsive, Seo, Wrapper };
